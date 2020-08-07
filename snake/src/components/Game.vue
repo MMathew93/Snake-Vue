7f3b3cb347d063d9fb916997e529328626b00350
lyndexer <mckinney.m93@gmail.com>
2020-08-06 17:13:26 -0700

    Set up snake to move, tho it shoots off the screen

--- snake/src/components/Game.vue
+++ snake/src/components/Game.vue
@@ -9,9 +9,32 @@
     name: 'Game',
     data() {
       return {
-        snake: [0, 1, 2]
+        snake: [{
+            x: 9 * 20,
+            y: 10 * 20
+          },
+          {
+            x: 8 * 20,
+            y: 10 * 20
+          },
+          {
+            x: 7 * 20,
+            y: 10 * 20
+          }
+        ],
+        direction: '',
+        startGame: false
       }
     },
+
+    watch: {
+      startGame(value) {
+        if (value) {
+          this.draw()
+        }
+      }
+    },
+
     methods: {
       //fix the canvas dpi for screen
       fix_dpi() {
@@ -26,38 +49,65 @@
         canvas.setAttribute('height', style_height * dpi);
         canvas.setAttribute('width', style_width * dpi);
       },
-    //draw the game
-    draw() {
-      const canvas = document.getElementById('canvas');
-      const snake = canvas.getContext('2d');
-      snake.fillStyle = 'green'
-      for(let i = 0; i < this.snake.length; i++ ) {
-        snake.fillStyle = 'green'
-        snake.fillRect([i].x, [i].y, 20, 20)
-      }
 
-      setInterval(this.draw, 1000)
-    },
+      //draw the game
+      draw() {
+        const canvas = document.getElementById('canvas');
+        const context = canvas.getContext('2d');
+        for (let i = 0; i < this.snake.length; i++) {
+          context.fillStyle = ( i == 0 ) ? "green" : "gray";
+          context.fillRect(this.snake[i].x, this.snake[i].y, 20, 20)
+        }
+        //head of the snake
+        let snakeX = this.snake[0].x;
+        let snakeY = this.snake[0].y;
 
-    //directions
-    directions(event) {
-      //event.keyCode 37 - 40
-      if(event.keyCode == 37) {
-        console.log('hello')
-      } else {
-        console.log('false')
-      }
-    }
+        if (this.direction === 'left') {
+          snakeX -= 20
+        }
+        if (this.direction === 'up') {
+          snakeY -= 20
+        }
+        if (this.direction === 'right') {
+          snakeX += 20
+        }
+        if (this.direction === 'down') {
+          snakeY += 20
+        }
 
+        //remove tail
+        this.snake.pop();
+
+        //add new head
+        let newHead = {
+          x: snakeX,
+          y: snakeY
+        }
+        this.snake.unshift(newHead);
+
+
+        setInterval(this.draw, 1000)
+      },
+
+      //directions
+      directions(event) {
+        this.startGame = true
+        //event.keyCode 37 - 40
+        if (event.keyCode == 37) {
+          this.direction = 'left'
+        } else if (event.keyCode == 38) {
+          this.direction = 'up'
+        } else if (event.keyCode == 39) {
+          this.direction = 'right'
+        } else if (event.keyCode == 40) {
+          this.direction = 'down'
+        }
+      },
     },
     mounted() {
       //draw the snake
       this.fix_dpi()
-      const canvas = document.getElementById('canvas');
-      const snake = canvas.getContext('2d');
-      snake.fillStyle = 'green'
-      snake.fillRect(100, 100, 20, 20);
-      document.addEventListener('keydown', this.directions())
+      document.addEventListener('keydown', this.directions)
     }
   }
 </script>
